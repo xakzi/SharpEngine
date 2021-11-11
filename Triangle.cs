46163614831eef4b29a95b541f7f537ad5abffcd
xakzi <emanoel.kouriat@student.forsbergsskola.se>
2021-11-11 22:50:18 +0100

    Did first task of excerciese

--- Triangle.cs
+++ Triangle.cs
@@ -1,4 +1,5 @@
-﻿using static OpenGL.Gl;
+﻿using System.Runtime.InteropServices;
+using static OpenGL.Gl;
 
 namespace SharpEngine
 {
@@ -9,6 +10,7 @@ namespace SharpEngine
         {
             this.vertices = vertices;
             currentScale = 1f;
+            LoadTriangleIntoBuffer();
         }
 
         public float currentScale { get; private set; }
@@ -53,6 +55,17 @@ namespace SharpEngine
             currentScale *= multiplier;
         }
 
+        public void Rotate()
+        {
+            var center = GetCenter();
+
+            Move(center * -1);
+
+            //rotate magic
+
+            Move(center);
+        }
+
         public void Move(Vector direction)
         {
             for (var i = 0; i < vertices.Length; i++)
@@ -66,6 +79,23 @@ namespace SharpEngine
                 glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.Length, vertex, GL_STATIC_DRAW);
             }
             glDrawArrays(GL_TRIANGLES, 0, vertices.Length);
+        }
+
+        private static unsafe void LoadTriangleIntoBuffer()
+        {
+            // load the vertices into a buffer
+            var vertexArray = glGenVertexArray();
+            var vertexBuffer = glGenBuffer();
+
+            glBindVertexArray(vertexArray);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.position)));
+            glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), (void*)(sizeof(Vector)));
+
+            glEnableVertexAttribArray(0);
+            glEnableVertexAttribArray(1);
         }
     }
 }
