a02205e1389879a4321fd5ff714f609ce752672a
xakzi <emanoel.kouriat@student.forsbergsskola.se>
2021-11-11 16:20:56 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,33 +2,39 @@
 using GLFW;
 using static OpenGL.Gl;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace SharpEngine
 {
+
     class Program
     {
+        static Triangle triangle = new Triangle
+        (
+            new Vertex[]
+            {
+                /*new Vertex(new Vector(-.1f, -.1f), Color.Red),
+                new Vertex(new Vector(.1f, -.1f), Color.Green),
+                new Vertex(new Vector(0f, .1f), Color.Blue),*/
 
-        static Vertex[] vertices = new Vertex[] {
-            new Vertex(new Vector(-.1f, -.1f), Color.Red),
-            new Vertex(new Vector(.1f, -.1f), Color.Green),
-            new Vertex(new Vector(0f, .1f), Color.Blue),
+                new Vertex(new Vector(.4f, .2f), Color.Purple),
+                new Vertex(new Vector(.6f, .2f), Color.Cyan),
+                new Vertex(new Vector(.5f, .4f), Color.Yellow)
+            }
+        );
 
-            new Vertex(new Vector(.4f, .2f), Color.Purple),
-            new Vertex(new Vector(.6f, .2f), Color.Cyan),
-            new Vertex(new Vector(.5f, .4f), Color.Yellow)
-            };
-        
         static float transformSpeed = 0.005f;
         static void Main(string[] args)
         {
-            
+
             // initialize and configure
             Window window = CreateWindow();
             LoadTriangleIntoBuffer();
             CreateShaderProgram();
             var direction = new Vector(transformSpeed, transformSpeed);
             var multiplier = 0.998f;
-            var scale = 1f;
+            var maxScale = 0.998f;
+            var minScale = 1.002f;
             // engine rendering loop
             while (!Glfw.WindowShouldClose(window))
             {
@@ -36,82 +42,35 @@ namespace SharpEngine
                 ClearScreen();
                 Render(window);
 
-                //Bounces
-                direction = TriangleBounceOnWall(direction);
+                triangle.Scale(multiplier);
 
-                //scaling
-                TriangleScaleUpAndDownContinously(ref multiplier, ref scale);
+                if (triangle.currentScale <= 0.5f)
+                    multiplier = minScale;
+                if (triangle.currentScale >= 1.5f)
+                    multiplier = maxScale;
 
-                UpdateTriangleBuffer();
-            }
-        }
 
-        private static Vector TriangleBounceOnWall(Vector direction)
-        {
-            // 3. Move the Triangle by its Direction
-            for (var i = 0; i < vertices.Length; i++)
-                vertices[i].position += direction;
+                // 3. Move the Triangle by its Direction
+                triangle.Move(direction);
 
-            // 4. Check the X-Bounds of the Screen
-            for (var i = 0; i < vertices.Length; i++)
-            {
-                if (vertices[i].position.x >= 1 && direction.x > 0 || vertices[i].position.x <= -1 && direction.x < 0)
+                // 4. Check the X-Bounds of the Screen
+                if(triangle.GetMaxBounds().x >= 1 && direction.x > 0 || triangle.GetMinBounds().x <= -1 && direction.x < 0)
                 {
                     direction.x *= -1;
-                    break;
                 }
-            }
-            // 5. Check the Y-Bounds of the Screen
-            for (var i = 0; i < vertices.Length; i++)
-            {
-                if (vertices[i].position.y >= 1 && direction.y > 0 || vertices[i].position.y <= -1 && direction.y < 0)
+
+                if (triangle.GetMaxBounds().y >= 1 && direction.y > 0 || triangle.GetMinBounds().y <= -1 && direction.y < 0)
                 {
                     direction.y *= -1;
-                    break;
                 }
+
             }
-
-            return direction;
-        }
-
-        private static void TriangleScaleUpAndDownContinously(ref float multiplier, ref float scale)
-        {
-            //move it to the center
-            // - finding the center of the triangle
-            var min = vertices[0].position;
-            for (var i = 0; i < vertices.Length; i++)
-                min = Vector.Min(min, vertices[i].position);
-
-            var max = vertices[2].position;
-            for (var i = 0; i < vertices.Length; i++)
-                max = Vector.Max(max, vertices[i].position);
-
-            var center = (min + max) / 2;
-
-            // - moving it by the opposite vector
-            for (var i = 0; i < vertices.Length; i++)
-                vertices[i].position -= center;
-
-            //scale the triangle
-            for (var i = 0; i < vertices.Length; i++)
-                vertices[i].position *= multiplier;
-
-            //move it back to where it was
-            for (var i = 0; i < vertices.Length; i++)
-                vertices[i].position += center;
-
-            scale *= multiplier;
-
-            if (scale <= 0.5f)
-                multiplier = 1.002f;
-            if (scale >= 1f)
-                multiplier = 0.998f;
         }
 
         private static void Render(Window window)
         {
             //glDrawArrays(GL_LINE_LOOP, 0, 3); //Lined Triangle
-            glDrawArrays(GL_TRIANGLES, 0, vertices.Length); //Filled Triangle
+            triangle.Render();
 
             Glfw.SwapBuffers(window);
             //glFlush();
@@ -124,7 +83,7 @@ namespace SharpEngine
         }
 
         private static unsafe void LoadTriangleIntoBuffer()
-        { 
+        {
             // load the vertices into a buffer
             var vertexArray = glGenVertexArray();
             var vertexBuffer = glGenBuffer();
@@ -132,21 +91,12 @@ namespace SharpEngine
             glBindVertexArray(vertexArray);
             glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 
-            UpdateTriangleBuffer();
 
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), NULL);
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.position)));
             glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), (void*)(sizeof(Vector)));
 
             glEnableVertexAttribArray(0);
             glEnableVertexAttribArray(1);
-        }
-
-        static unsafe void UpdateTriangleBuffer()
-        {
-            fixed (Vertex* vertex = &vertices[0])
-            {
-                glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.Length, vertex, GL_STATIC_DRAW);
-            }
         }
 
         private static void CreateShaderProgram()
