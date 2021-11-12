ebdca84c1f235e48fc8488fb9cbc8bb490f8aa1b
xakzi <emanoel.kouriat@student.forsbergsskola.se>
2021-11-12 17:17:18 +0100

    Added a new Class

--- Triangle.cs
+++ Triangle.cs
@@ -3,14 +3,16 @@ using System.Runtime.InteropServices;
 using OpenGL;
 using GLFW;
 using static OpenGL.Gl;
-/*
+
 namespace SharpEngine
 {
     class Triangle : Shape
     {
-        public Triangle(float width, float height, Vector position) : base()
+        public Triangle(float width, float height, Vector position) : base(new Vertex[3])
         {
-
+            vertices[0] = new Vertex(new Vector(position.x - width / 2, position.y - height / 2), Color.Red);
+            vertices[1] = new Vertex(new Vector(position.x + width / 2, position.y - height / 2), Color.Pink);
+            vertices[2] = new Vertex(new Vector(position.x, position.y + height / 2), Color.Blue);
         }
     }
-}*/
+}
