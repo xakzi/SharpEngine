03722d8b51b69be9e91cad0a41698c3024c1be3d
xakzi <emanoel.kouriat@student.forsbergsskola.se>
2021-11-12 23:49:11 +0100

    Fixed so first vertices is in the middle for the color lol

--- Circle.cs
+++ Circle.cs
@@ -8,21 +8,21 @@ namespace SharpEngine
 {
     class Circle : Shape
     {
-        public Circle(float radius, Vector position) : base(new Vertex[36])
+        public Circle(float radius, Vector position) : base(new Vertex[48])
         {
-            float theta = MathF.PI * 2 / 36;
 
-            Color newColor = Color.Aqua;
-            vertices[0].position = position;
-            //vertices[0].color = Color.Blue;
-            for (var i = 0; i < vertices.Length; i++)
-            {
-                if (i % 9 < 3) newColor = Color.Red;
-                else if (i % 9 >= 3 && i % 9 < 6) newColor = Color.Pink;
-                else newColor = Color.Blue;
+            float theta = MathF.PI * 2 / (48 - 2);
 
-                vertices[i] = new Vertex(new Vector(radius * MathF.Cos(theta * (i - 1))+position.x, radius * MathF.Sin(theta * (i - 1))+position.y), newColor);
-            }
+                Color newColor;
+                vertices[0].position = position;
+                for (var i = 0; i < vertices.Length; i++)
+                {
+                    if (i % 9 < 3) newColor = Color.Red;
+                    else if (i % 9 >= 3 && i % 9 < 6) newColor = Color.Pink;
+                    else newColor = Color.Aqua;
+
+                    vertices[i] = new Vertex(new Vector(radius * MathF.Cos(theta * (i - 1)) + vertices[0].position.x, radius * MathF.Sin(theta * (i - 1)) + vertices[0].position.y), newColor);
+                }
         }
     }
 }
