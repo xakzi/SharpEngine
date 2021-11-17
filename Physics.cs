8adc777b69e0f30e3fa91837d76bdf5ee73ef97f
xakzi <emanoel.kouriat@student.forsbergsskola.se>
2021-11-17 13:47:54 +0100

    Added Physics

---
+++ Physics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GLFW;
+using OpenGL;
+
+namespace SharpEngine
+{
+    class Physics
+    {
+        readonly Scene scene;
+        public Physics(Scene scene)
+        {
+            this.scene = scene;
+        }
+
+        public void Update(float deltaTime)
+        {
+            for (int i = 0; i < this.scene.shapes.Count; i++)
+            {
+                Shape shape = this.scene.shapes[i];
+                // linear velocity
+                shape.Transform.Position = shape.Transform.Position + shape.velocity * deltaTime;
+                // a = F/m (another version of F = m * a
+                var acceleration = shape.linearForce / shape.mass;
+                // linear acceleration:
+                shape.Transform.Position = shape.Transform.Position + acceleration * deltaTime * deltaTime / 2;
+                shape.velocity = shape.velocity + acceleration * deltaTime;
+            }
+        }
+    }
+}
