45c05474fa99fdbef867a854310be7d48c672fe9
xakzi <emanoel.kouriat@student.forsbergsskola.se>
2021-11-17 13:48:02 +0100

    Added Square

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace SharpEngine
             var window = new Window();
             var material = new Material("shaders/world-position-color.vert", "shaders/vertex-color.frag");
             var scene = new Scene();
+            var physics = new Physics(scene);
             window.Load(scene);
 
             const int fixedStepNumberPerSecond = 30;
@@ -28,91 +29,30 @@ namespace SharpEngine
             double previousFixedStep = 0.0;
             const float movementSpeed = 1f;
 
-            var triangle = new Triangle(material);
-            scene.Add(triangle);
-
-            var rectangle = new Rectangle(material);
-            rectangle.Transform.Position = new Vector(-.5f, -.5f);
-            scene.Add(rectangle);
-
             var circle = new Circle(material);
-            circle.Transform.Position = new Vector(-.5f, .5f);
+            circle.Transform.Position = Vector.Left;
+            circle.velocity = Vector.Right * 0.3f;
             scene.Add(circle);
 
-            var ground1 = new Rectangle(material);
-            ground1.Transform.CurrentScale = new Vector(20f, 1f, 1f);
-            ground1.Transform.Position = new Vector(0f, -1f);
-            scene.Add(ground1);
+            var square = new Rectangle(material);
+            square.Transform.Position = Vector.Left + Vector.Backward * 0.2f;
+            square.linearForce = Vector.Right * 0.3f;
+            square.mass *= 4f;
+            scene.Add(square);
 
-            // engine rendering loop
+            var ground = new Rectangle(material);
+            ground.Transform.CurrentScale = new Vector(20f, 1f, 1f);
+            ground.Transform.Position = new Vector(0f, -1f);
+            scene.Add(ground);
+
             while (window.IsOpen())
             {
                 while (Glfw.Time > previousFixedStep + fixedDeltaTime)
                 {
                     previousFixedStep += fixedDeltaTime;
-                    var walkDirection = new Vector();
-
-                    if (window.GetKey(Keys.W))
-                    {
-                        walkDirection += triangle.Transform.Forward;
-                        //shape.Transform.Position += new Vector(0f, movementSpeed * fixedDeltaTime);
-                    }
-                    if (window.GetKey(Keys.S))
-                    {
-                        walkDirection += triangle.Transform.Backward;
-                        //shape.Transform.Position += new Vector(0f, -movementSpeed * fixedDeltaTime);
-                    }
-                    if (window.GetKey(Keys.A))
-                    {
-                        //walkDirection += Vector.Left;
-                        //shape.Transform.Position += new Vector(-movementSpeed * fixedDeltaTime, 0 );
-                        var rotation = triangle.Transform.Rotation;
-                        rotation.z += 2 * MathF.PI * fixedDeltaTime;
-                        triangle.Transform.Rotation = rotation;
-                    }
-                    if (window.GetKey(Keys.D))
-                    {
-                        //walkDirection += Vector.Right;
-                        //shape.Transform.Position += new Vector(movementSpeed * fixedDeltaTime, 0);
-                        var rotation = triangle.Transform.Rotation;
-                        rotation.z -= 2 * MathF.PI * fixedDeltaTime;
-                        triangle.Transform.Rotation = rotation;
-                    }
-
-
-
-                    walkDirection = walkDirection.Normalize();
-                    triangle.Transform.Position += walkDirection * (movementSpeed * fixedDeltaTime);
-                    
-                    
-                    CircleChangeColor(triangle, circle);
-
-                    RectangleChangeColor(triangle, rectangle);
-
+                    physics.Update(fixedDeltaTime);
                 }
                 window.Render();
-            }
-        }
-
-        private static void CircleChangeColor(Triangle triangle, Circle circle)
-        {
-            var angle = MathF.Acos(Vector.Dot((circle.GetCenter() - triangle.GetCenter()).Normalize(), triangle.Transform.Forward));
-            float interpolate = angle / MathF.PI;
-            circle.SetColor(new Color(interpolate, interpolate, interpolate, 1));
-        }
-
-        private static void RectangleChangeColor(Triangle triangle, Rectangle rectangle)
-        {
-            float direction = Vector.Dot((rectangle.GetCenter() - triangle.GetCenter()).Normalize(), triangle.Transform.Forward);
-            
-            if (direction < 0) // change color on triangles left and right side
-            //if (Vector.Dot(rectangle.Transform.Forward, triangle.Transform.Forward) < 0) //change color on triangles right side
-            {
-                rectangle.SetColor(Color.Green);
-            }
-            else
-            {
-                rectangle.SetColor(Color.Red);
             }
         }
     }
